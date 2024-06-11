33a7f4d20553c3f970be4e5ce4f25b92c16ab23e
Aleksei <aleksei.zh@mail.ru>
2024-06-11 13:43:12 +0300

    Ver 2.1

---
+++ src/main/java/org/example/config/ConfigurationSettings.java
@@ -0,0 +1,26 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.example.controller.PostController;
+import org.example.repository.PostRepository;
+import org.example.service.PostService;
+
+@Configuration
+public class ConfigurationSettings {
+
+    @Bean
+    public PostController postController(PostService postService) {
+        return new PostController(postService);
+    }
+
+    @Bean
+    public PostService postService(PostRepository postRepository) {
+        return new PostService(postRepository);
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepository();
+    }
+}
